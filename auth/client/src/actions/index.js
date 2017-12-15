95f15cc4f5de51b9cf7e4df9d0372ee3ef292afe
Ashot Israelyan <ashot.israelyan@picsart.com>
2017-12-15 12:41:01 +0400

    Authentication finished

--- auth/client/src/actions/index.js
+++ auth/client/src/actions/index.js
@@ -3,7 +3,8 @@ import { browserHistory } from 'react-router';
 import {
     AUTH_USER,
     UNAUTH_USER,
-    AUTH_ERROR
+    AUTH_ERROR,
+    FETCH_MESSAGE
 } from './types';
 
 const ROOT_URL = 'http://localhost:3090';
@@ -33,9 +34,9 @@ export function signupUser({ email, password }) {
     return function (dispatch) {
         axios.post(`${ROOT_URL}/signup`, { email, password })
             .then(response => {
-               dispatch({ type: AUTH_USER });
-               localStorage.setItem('token', response.data.token);
-               browserHistory.push('/feature');
+                dispatch({ type: AUTH_USER });
+                localStorage.setItem('token', response.data.token);
+                browserHistory.push('/feature');
             })
             .catch(response => dispatch(authError(response.data.error)));
     }
@@ -52,4 +53,18 @@ export function signoutUser() {
     localStorage.removeItem('token');
 
     return { type: UNAUTH_USER };
+}
+
+export function fetchMessage() {
+    return function (dispatch) {
+        axios.get(ROOT_URL, {
+            headers: { authorization: localStorage.getItem('token') }
+        })
+            .then(response => {
+                dispatch({
+                    type: FETCH_MESSAGE,
+                    payload: response.data.message
+                })
+            });
+    }
 }
